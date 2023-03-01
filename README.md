98526cd10fbe6b6853788543b01ac35c41afe161
Dhanajit Kapali <dhanajit.k@peopleinteractive.in>
2023-03-01 14:41:55 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PreventScreenCaptureKit
+
+A description of this package.
---
+++ Sources/PreventScreenCaptureKit/PreventScreenCaptureKit.swift
@@ -0,0 +1,6 @@
+public struct PreventScreenCaptureKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
