fa161863a551f0b414ef74666b156c22a5fa0d96
Dhanajit Kapali <dhanajitkapali30@gmail.com>
2024-04-09 16:35:25 +0530

    Fixed some Indentation

--- Sources/PreventScreenCaptureKit/ViewModifiers/PreventScreenCaptureViewModifier.swift
+++ Sources/PreventScreenCaptureKit/ViewModifiers/PreventScreenCaptureViewModifier.swift
@@ -11,7 +11,14 @@ public struct PreventScreenCaptureViewModifier: ViewModifier {
     @available(iOS 13.0.0, *)
     public func body(content: Content) -> some View {
         GeometryReader { reader in
-            PreventScreenCaptureView(frame: .init(x: 0, y: 0, width: reader.size.width, height: reader.size.height)) {
+            PreventScreenCaptureView(
+                frame: .init(
+                    x: 0,
+                    y: 0,
+                    width: reader.size.width,
+                    height: reader.size.height
+                )
+            ) {
                 content
             }
         }
